0cc9eb3bca1bf9e76f852160db5ab53511ad0995
Gabriel da Gama <gabriel.dagama@origin8.ie>
2019-11-20 18:10:10 +0000

    Removing unnecessary import

--- src/main/java/com/jtweet/tweetmanagement/controller/api/v1/TweetController.java
+++ src/main/java/com/jtweet/tweetmanagement/controller/api/v1/TweetController.java
@@ -1,7 +1,5 @@
 package com.jtweet.tweetmanagement.controller.api.v1;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
