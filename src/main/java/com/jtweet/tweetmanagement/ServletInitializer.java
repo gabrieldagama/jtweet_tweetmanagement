7cb3a4fe4df79bc1d7444bdbdd9ca6bf82eb9fbf
Gabriel da Gama <gabriel.dagama@origin8.ie>
2019-11-29 15:06:18 +0000

    Converted to war output

---
+++ src/main/java/com/jtweet/tweetmanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jtweet.tweetmanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TweetManagementApplication.class);
+	}
+
+}
